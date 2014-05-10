b70a48478f91bf380c50f8e459a158c3212ad8f7
Josh Toft <josh@ooyala.com>
2014-05-10 00:14:26 -0700

    Add short and skips to s3 storage test

--- src/registry/storage/s3_test.go
+++ src/registry/storage/s3_test.go
@@ -7,6 +7,16 @@ import (
 
 func TestS3(t *testing.T) {
 	// read test config. has sensitive data so pass filename in as env variable
+	if testing.Short() {
+		t.Skip("skipping s3 tests in short mode.")
+	}
+
+	s3_cfg_file := os.Getenv("TEST_S3_CONFIG")
+	if _, err := os.Stat(s3_cfg_file); os.IsNotExist(err) {
+		skip_msg := "skipping s3 tests because the config file `%s` does not exist."
+		t.Skip(skip_msg, s3_cfg_file)
+	}
+
 	var s3 S3
 	err := storageFromFile(os.Getenv("TEST_S3_CONFIG"), &s3)
 	if err != nil {
